a25b3e5ac7410c0ad39e786d617624decdcbc92e
the-hasib <hasib.cse.uiu@gmail.com>
2025-07-25 22:38:47 +0600

    Added C++ code to sum two 2-digit user inputs

---
+++ 2digit_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    // Taking input from the user
+    cout << "Enter the first number: ";
+    cin >> n1;
+
+    cout << "Enter the second number: ";
+    cin >> n2;
+
+   //calculate sum
+   int sum =n1+n2;
+   cout<<"result"<<sum<<endl;
+
+    return 0;
+}
