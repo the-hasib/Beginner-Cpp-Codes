3fc0f008eaf3412c172f9757db96d163e141632d
the-hasib <hasib.cse.uiu@gmail.com>
2025-10-04 23:18:21 +0600

    Add a code

---
+++ Pyramid/Half_Diamond_Pattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // Upper part
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower part
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
