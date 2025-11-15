5d232923b993c1d3437cf2975682de254c0c3535
the-hasib <hasib.cse.uiu@gmail.com>
2025-11-15 23:32:27 +0600

    Add a code

---
+++ Pyramid/Hollow_Hourglass_Pattern.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+
+    // Outer loop to iterate through each row
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+
+        // Assigning comparator
+        int comp;
+        if (i < n)
+            comp = 2 * i + 1;
+        else
+            comp = 2 * (2 * n - i) - 3;
+
+        // First inner loop to print leading whitespaces
+        for (int j = 0; j < comp; j++)
+            printf(" ");
+
+        // Second inner loop to print star * and inner whitespaces
+        for (int k = 0; k < 2 * n - comp; k++)
+        {
+            if (k == 0 || k == 2 * n - comp - 1 || i == 0 || i == 2 * n - 2)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
